e3513d4439d3ef9d7c8a5e2a1677158c85a7f03a
Jakub Bocheński <jakub.bochenski@f-secure.com>
2019-03-01 15:25:45 +0100

    sample project

---
+++ helloworld/src/main/java/io/fabric8/dmp/sample/helloworld/App.java
@@ -0,0 +1,11 @@
+package io.fabric8.dmp.sample.helloworld;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(40000);
+        System.out.println("Hello World!");
+    }
+}
